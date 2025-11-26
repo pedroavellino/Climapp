137c2cb9639d0ba9e10630371b43f25eaace12a3
Pedro Avelino <pedro_avelino_@hotmail.com>
2025-11-26 10:38:42 -0300

    feat(app): development of the ScrollView and addition of icons

--- app/cities.tsx
+++ app/cities.tsx
@@ -1,5 +1,5 @@
 import { LinearGradient } from "expo-linear-gradient";
-import { StyleSheet, Text, View } from "react-native";
+import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
 import citiesData from "../data/cities.json"
 
 const Cities = () => {
@@ -9,17 +9,27 @@ const Cities = () => {
       colors={["#00457d", "#05051f"]}
       style={styles.container}
     >
-      {
-        citiesData.map((city) => (
-          <View style={styles.listItem}>
-            <Text
-              style={styles.cityName}
-            >
-              {city.city}
-            </Text>
-          </View>
-        ))
-      }
+      <ScrollView>
+        <View style={styles.scrollList}>
+          {
+            citiesData.map((city) => (
+              <View style={styles.listItem}>
+                <Image source={require("../assets/images/clouds.png")}/>
+                <Text
+                  style={styles.cityName}
+                >
+                  {city.city}
+                </Text>
+                <Text
+                  style={styles.cityTemp}
+                >
+                  {city.temp}
+                </Text>
+              </View>
+            ))
+          }
+        </View>
+      </ScrollView>
     </LinearGradient>
   )
 }
@@ -28,22 +38,30 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingHorizontal: 16,
+  },
+  scrollList: {
     gap: 16,
   },
   listItem: {
     width: "100%",
-    backgroundColor: "rgba(255, 255, 255, 0.15)",
-    justifyContent: "center",
+    flexDirection: "row",
+    justifyContent: "space-between",
     alignItems: "center",
-    borderRadius: 16,
     paddingVertical: 21,
-  
+    paddingHorizontal: 16,
+    backgroundColor: "rgba(255, 255, 255, 0.15)",
+    borderRadius: 16,
   },
   cityName: {
     color: "#FFFFFF",
     fontSize: 16,
     fontFamily: "MontSerrat_500Medium",
-  }
+  },
+  cityTemp: {
+    color: "#FFFFFF",
+    fontSize: 25,
+    fontFamily: "Montserrat_700Bold",
+  },
 });
 
 export default Cities;

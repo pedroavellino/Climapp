fe68007bca134afd21574564b0b5238374632871
Pedro Avelino <pedro_avelino_@hotmail.com>
2025-12-14 12:08:58 -0300

    feat(app): start of API intgration

--- app/[cityName].tsx
+++ app/[cityName].tsx
@@ -1,11 +1,28 @@
 import { useLocalSearchParams } from "expo-router";
+import { useEffect } from "react";
 import { View } from "react-native"
 
 const CityDetails = () => {
 
-  const searcjhParams = useLocalSearchParams();
+  const searchParams = useLocalSearchParams();
 
-  console.log(searcjhParams);
+  const handleData = async () => {
+    try {
+      const response = await fetch("https://climapp-api.vercel.app/api");
+      const responseJSON = await response.json()
+      const city = responseJSON.find(cityData => 
+        cityData.city === searchParams.cytyName
+      )
+
+      console.log(city)
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+    useEffect(() => {
+      handleData();
+    }, [])
 
   return (
     <View>
